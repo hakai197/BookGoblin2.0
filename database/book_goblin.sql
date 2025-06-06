fce72a1dd3c98ab785213d33de65eeb4f2a4911a
Nicholas Mathias <hakai197@gmail.com>
2025-06-06 11:46:43 -0400

    database started

---
+++ database/book_goblin.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    user_id SERIAL,
+    username varchar(50) NOT NULL UNIQUE,
+    password_hash varchar(200) NOT NULL,
+    role varchar(50) NOT NULL,
+    CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+-- Insert Users
+-- Password for all users is password
+INSERT INTO users (username, password_hash, role) VALUES
+('user', '$2a$10$tmxuYYg1f5T0eXsTPlq/V.DJUKmRHyFbJ.o.liI1T35TFbjs2xiem', 'ROLE_USER'),
+('admin', '$2a$10$tmxuYYg1f5T0eXsTPlq/V.DJUKmRHyFbJ.o.liI1T35TFbjs2xiem', 'ROLE_ADMIN');
